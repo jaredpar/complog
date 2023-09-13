e3f228dc798385efc91085ecf5334d2c2276170c
Jared Parsons <jared@paranoidcoding.org>
2023-09-13 15:07:53 -0700

    Simple workspace

--- src/Basic.CompilerLog.Util/SolutionReader.cs
+++ src/Basic.CompilerLog.Util/SolutionReader.cs
@@ -54,6 +54,13 @@ public sealed class SolutionReader : IDisposable
         return new(CompilerLogReader.Create(stream, leaveOpen: false, options), predicate);
     }
 
+    public AdhocWorkspace CreateWorkspace()
+    {
+        var workspace = new AdhocWorkspace();
+        _ = workspace.AddSolution(ReadSolutionInfo());
+        return workspace;
+    }
+
     public SolutionInfo ReadSolutionInfo()
     {
         var projectInfoList = new List<ProjectInfo>();
