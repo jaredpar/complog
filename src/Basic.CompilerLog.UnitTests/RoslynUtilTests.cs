21d78f42fc0231a1b8e9b2a56c90084882ad0923
Jared Parsons <jared@paranoidcoding.org>
2024-01-29 20:25:12 -0800

    Detect global editor config from SourceText

--- src/Basic.CompilerLog.UnitTests/RoslynUtilTests.cs
+++ src/Basic.CompilerLog.UnitTests/RoslynUtilTests.cs
@@ -1,5 +1,6 @@
 using Basic.CompilerLog.Util;
 using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Text;
 using Microsoft.CodeAnalysis.VisualBasic;
 using Xunit;
 
@@ -7,6 +8,77 @@ namespace Basic.CompilerLog.UnitTests;
 
 public sealed class RoslynUtilTests
 {
+    public static IEnumerable<object[]> GetIsGlobalConfigData()
+    {
+        yield return new object[]
+        { 
+            true,
+            """
+            is_global = true
+            """
+        };
+
+        yield return new object[]
+        { 
+            false,
+            """
+            is_global = false
+            """
+        };
+
+        yield return new object[]
+        { 
+            true,
+            """
+            is_global = false
+            is_global = true
+            """
+        };
+
+        // Don't read past the first section
+        yield return new object[]
+        { 
+            false,
+            """
+            [c:\example.cs]
+            is_global = false
+            is_global = true
+            """
+        };
+
+        // ignore comments
+        yield return new object[]
+        { 
+            false,
+            """
+            ;is_global = true
+            a = 3
+            """
+        };
+
+        // Super long lines
+        yield return new object[]
+        { 
+            false,
+            $"""
+            ;{new string('a', 1000)}
+            ;is_global = true
+            a = 3
+            """
+        };
+
+        // Super long lines
+        yield return new object[]
+        { 
+            true,
+            $"""
+            ;{new string('a', 1000)}
+            is_global = true
+            a = 3
+            """
+        };
+    }
+
     [Fact]
     public void ParseAllVisualBasicEmpty()
     {
@@ -19,5 +91,14 @@ public sealed class RoslynUtilTests
     {
         var result = RoslynUtil.ParseAllCSharp([], CSharpParseOptions.Default);
         Assert.Empty(result);
+    }
+
+    [Theory]
+    [MemberData(nameof(GetIsGlobalConfigData))]
+    public void IsGlobalConfig(bool expected, string contents)
+    {
+        var sourceText = SourceText.From(contents);
+        var actual = RoslynUtil.IsGlobalEditorConfig(sourceText);
+        Assert.Equal(expected, actual);
     }
 }
