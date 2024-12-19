87b2150001607d9bbedd02fdd966b0e60bf5f12d
Jared Parsons <jared@paranoidcoding.org>
2024-12-18 16:56:45 -0800

    Move to dynamically skippable tests

--- src/Basic.CompilerLog.UnitTests/ConditionalFacts.cs
+++ src/Basic.CompilerLog.UnitTests/ConditionalFacts.cs
@@ -5,12 +5,13 @@ using Xunit;
 
 public sealed class WindowsFactAttribute : FactAttribute
 {
+    public static bool IsWindows => RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+
     public WindowsFactAttribute()
     {
-        if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-        {
-            Skip = "This test is only supported on Windows";
-        }
+        SkipUnless = nameof(WindowsFactAttribute.IsWindows);
+        SkipType = typeof(WindowsFactAttribute);
+        Skip = "This test is only supported on Windows";
     }
 }
 
@@ -18,21 +19,21 @@ public sealed class WindowsTheoryAttribute : TheoryAttribute
 {
     public WindowsTheoryAttribute()
     {
-        if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-        {
-            Skip = "This test is only supported on Windows";
-        }
+        SkipUnless = nameof(WindowsFactAttribute.IsWindows);
+        SkipType = typeof(WindowsFactAttribute);
+        Skip = "This test is only supported on Windows";
     }
 }
 
 public sealed class UnixTheoryAttribute : TheoryAttribute
 {
+    public static bool IsUnix => !RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+
     public UnixTheoryAttribute()
     {
-        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-        {
-            Skip = "This test is only supported on Windows";
-        }
+        Skip = "This test is only supported on Unix";
+        SkipUnless = nameof(IsUnix);
+        SkipType = typeof(UnixTheoryAttribute);
     }
 }
 
