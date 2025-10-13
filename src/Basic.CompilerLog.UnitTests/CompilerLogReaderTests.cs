a3e6e3dfd63c4fbab111349cacbe7cd920d91a79
Jared Parsons <jared@paranoidcoding.org>
2025-10-12 21:21:41 -0700

    More

--- src/Basic.CompilerLog.UnitTests/CompilerLogReaderTests.cs
+++ src/Basic.CompilerLog.UnitTests/CompilerLogReaderTests.cs
@@ -1,4 +1,4 @@
-﻿using Basic.CompilerLog.Util;
+using Basic.CompilerLog.Util;
 using Basic.CompilerLog.Util.Impl;
 using Microsoft.Build.Logging.StructuredLogger;
 using Microsoft.CodeAnalysis;
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.Design.Serialization;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.IO.Compression;
 using System.IO.Pipelines;
 using System.Linq;
@@ -107,6 +108,22 @@ public sealed class CompilerLogReaderTests : TestBase
             using var zip = new ZipArchive(fileStream, ZipArchiveMode.Create);
         }
         Assert.Throws<Exception>(() => CompilerLogReader.Create(zipFilePath));
+    }
+
+    [Fact]
+    public void GetContentBytes()
+    {
+        using var reader = CompilerLogReader.Create(Fixture.ConsoleComplex.Value.CompilerLogPath);
+        reader.PathNormalizationUtil = new IdentityPathNormalizationUtil();
+        var compilerCall = reader.ReadCompilerCall(0);
+        var any = false;
+        foreach (var rawContent in reader.ReadAllRawContent(compilerCall, RawContentKind.AnalyzerConfig))
+        {
+            var bytes = reader.GetContentBytes(rawContent.Kind, rawContent.ContentHash!);
+            Assert.NotEmpty(bytes);
+            any = true;
+        }
+        Assert.True(any);
     }
 
     [Fact]
@@ -687,3 +704,14 @@ public sealed class CompilerLogReaderTests : TestBase
         }
     }
 }
+
+/// <summary>
+/// This is a no-op path normalizer but it being a custom type means the reader avoids a lot of
+/// optimizations it would otherwise hit.
+/// </summary>
+file sealed class IdentityPathNormalizationUtil : PathNormalizationUtil
+{
+    internal override bool IsPathRooted([NotNullWhen(true)] string? path) => Empty.IsPathRooted(path);
+    internal override string RootFileName(string fileName) => Empty.RootFileName(fileName);
+    internal override string? NormalizePath(string? path) => Empty.NormalizePath(path);
+    }
