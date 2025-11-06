8cf44fcd93858ed8bd3f4e295d38039ee736ee26
Jan Jones <janjones@microsoft.com>
2025-11-06 16:21:24 +0100

    Include diagnostics in CompilerLogException message

--- src/Basic.CompilerLog.Util/CompilerLogException.cs
+++ src/Basic.CompilerLog.Util/CompilerLogException.cs
@@ -1,8 +1,6 @@
-using Microsoft.Build.Logging.StructuredLogger;
-
 namespace Basic.CompilerLog.Util;
 
-public sealed class CompilerLogException(string message, List<string>? diagnostics = null) : Exception(message)
+public sealed class CompilerLogException(string message, List<string>? diagnostics = null) : Exception(message + (diagnostics is [..] ? ("\n" + string.Join("\n", diagnostics)) : ""))
 {
     public IReadOnlyList<string> Diagnostics { get; } = diagnostics ?? (IReadOnlyList<string>)Array.Empty<string>();
 }
