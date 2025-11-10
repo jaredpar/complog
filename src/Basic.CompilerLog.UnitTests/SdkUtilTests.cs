fdcca7e3a358bf7a16cca7a3e75b729127c4dc7b
Jan Jones <janjones@microsoft.com>
2025-11-10 23:20:38 +0100

    Handle pre-release SDK versions (#297)

--- src/Basic.CompilerLog.UnitTests/SdkUtilTests.cs
+++ src/Basic.CompilerLog.UnitTests/SdkUtilTests.cs
@@ -1,5 +1,5 @@
-
 using Basic.CompilerLog.Util;
+using NuGet.Versioning;
 using Xunit;
 using Xunit.Sdk;
 
@@ -11,5 +11,24 @@ public sealed class SdkUtilTests
     public void GetDotnetDirectoryBadPath()
     {
         Assert.Throws<Exception>(() => SdkUtil.GetDotnetDirectory(@"C:\"));
+    }
+
+    [Fact]
+    public void SdkVersions()
+    {
+        using var temp = new TempDir();
+        var a = Path.GetFullPath(Path.Combine(temp.NewDirectory("sdk/9.0.100/Roslyn/bincore"), "../.."));
+        var b = Path.GetFullPath(Path.Combine(temp.NewDirectory("sdk/10.0.100-rc.2.25502.107/Roslyn/bincore"), "../.."));
+        temp.NewDirectory("sdk/invalid-version");
+        var sdks = SdkUtil.GetSdkDirectoriesAndVersion(temp.DirectoryPath);
+        Assert.Equal(
+            [
+                (a, new NuGetVersion(9, 0, 100)),
+                (b, new NuGetVersion(10, 0, 100, "rc.2.25502.107")),
+            ],
+            sdks.OrderBy(t => t.SdkVersion));
+
+        var latestSdk = SdkUtil.GetLatestSdkDirectories(temp.DirectoryPath);
+        Assert.Equal((b, new NuGetVersion(10, 0, 100, "rc.2.25502.107")), latestSdk);
     }
 }
