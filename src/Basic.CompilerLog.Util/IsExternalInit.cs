50c0821d76d420cd43f7b36071729bcbf39e60e8
Jared Parsons <jared@paranoidcoding.org>
2026-02-08 09:45:39 -0800

    Move polyfills into PolyfillTypes

--- src/Basic.CompilerLog.Util/IsExternalInit.cs
+++
@@ -1,6 +0,0 @@
-// Polyfill for record types on older target frameworks.
-namespace System.Runtime.CompilerServices;
-
-internal static class IsExternalInit
-{
-}
--- src/Basic.CompilerLog.Util/PolyfillTypes.cs
+++ src/Basic.CompilerLog.Util/PolyfillTypes.cs
@@ -1,45 +1,34 @@
-﻿#if !NET
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-
-namespace System.Diagnostics.CodeAnalysis;
-
-/// <summary>Specifies that when a method returns <see cref="ReturnValue"/>, the parameter will not be null even if the corresponding type allows it.</summary>
-[ExcludeFromCodeCoverage]
-[AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
-public sealed class NotNullWhenAttribute : Attribute
+// Polyfills for older target frameworks.
+#if NET472 || NETSTANDARD2_0
+namespace System.Runtime.CompilerServices
 {
-    /// <summary>Initializes the attribute with the specified return value condition.</summary>
-    /// <param name="returnValue">
-    /// The return value condition. If the method returns this value, the associated parameter will not be null.
-    /// </param>
-    public NotNullWhenAttribute(bool returnValue) => ReturnValue = returnValue;
-
-    /// <summary>Gets the return value condition.</summary>
-    public bool ReturnValue { get; }
+    internal static class IsExternalInit
+    {
+    }
 }
 
-/// <summary>Specifies that the output will be non-null if the named parameter is non-null.</summary>
-[ExcludeFromCodeCoverage]
-[AttributeUsage(AttributeTargets.Parameter | AttributeTargets.Property | AttributeTargets.ReturnValue, AllowMultiple = true, Inherited = false)]
-public sealed class NotNullIfNotNullAttribute : Attribute
+namespace System.Diagnostics.CodeAnalysis
 {
-    /// <summary>Initializes the attribute with the associated parameter name.</summary>
-    /// <param name="parameterName">
-    /// The associated parameter name.  The output will be non-null if the argument to the parameter specified is non-null.
-    /// </param>
-    public NotNullIfNotNullAttribute(string parameterName) => ParameterName = parameterName;
+    [AttributeUsage(AttributeTargets.Parameter | AttributeTargets.Property | AttributeTargets.ReturnValue, Inherited = false)]
+    internal sealed class NotNullIfNotNullAttribute : Attribute
+    {
+        public NotNullIfNotNullAttribute(string parameterName)
+        {
+            ParameterName = parameterName;
+        }
 
-    /// <summary>Gets the associated parameter name.</summary>
-    public string ParameterName { get; }
+        public string ParameterName { get; }
+    }
+
+    [AttributeUsage(AttributeTargets.Parameter | AttributeTargets.Property | AttributeTargets.ReturnValue, Inherited = false)]
+    internal sealed class NotNullWhenAttribute : Attribute
+    {
+        public NotNullWhenAttribute(bool returnValue)
+        {
+            ReturnValue = returnValue;
+        }
+
+        public bool ReturnValue { get; }
+    }
 }
-
 #endif
-
