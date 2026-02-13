24fcedf597fcf9b7d9ab009f1cd5aa6f69d4b1c7
Jared Parsons <jared@paranoidcoding.org>
2026-02-13 13:34:53 -0800

    tests

--- src/Basic.CompilerLog.UnitTests/SdkUtilTests.cs
+++ src/Basic.CompilerLog.UnitTests/SdkUtilTests.cs
@@ -1,6 +1,7 @@
 using Basic.CompilerLog.Util;
 using Xunit;
 using Xunit.Sdk;
+using static Basic.CompilerLog.Util.RoslynUtil;
 
 namespace Basic.CompilerLog.UnitTests;
 
@@ -16,9 +17,9 @@ public sealed class SdkUtilTests
     public void SdkVersions()
     {
         using var temp = new TempDir();
-        var a = Path.GetFullPath(Path.Combine(temp.NewDirectory("sdk/9.0.100/Roslyn/bincore"), "../.."));
-        var b = Path.GetFullPath(Path.Combine(temp.NewDirectory("sdk/10.0.100-rc.2.25502.107/Roslyn/bincore"), "../.."));
-        temp.NewDirectory("sdk/invalid-version");
+        var a = MakeSdk("9.0.100");
+        var b = MakeSdk("10.0.100-rc.2.25502.107");
+        var c = temp.NewDirectory("sdk/invalid-version");
         var sdks = SdkUtil.GetSdkDirectories(temp.DirectoryPath);
         Assert.Equal(
             [
@@ -29,5 +30,15 @@ public sealed class SdkUtilTests
 
         var latestSdk = SdkUtil.GetLatestSdkDirectory(temp.DirectoryPath);
         Assert.Equal((b, new SdkVersion(10, 0, 100, "rc.2.25502.107")), latestSdk);
+
+        string MakeSdk(string version)
+        {
+            var sdkPath = Path.Combine(temp.DirectoryPath, "sdk", version);
+            var compilerPath = Path.Combine(sdkPath, "Roslyn", "bincore");
+            _ = Directory.CreateDirectory(compilerPath);
+            File.WriteAllLines(Path.Combine(compilerPath, GetCompilerAppFileName(isCSharp: true)), []);
+            File.WriteAllLines(Path.Combine(compilerPath, GetCompilerAppFileName(isCSharp: false)), []);
+            return sdkPath;
+        }
     }
 }
